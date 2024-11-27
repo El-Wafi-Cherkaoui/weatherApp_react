469a3e3c565f2b30846d25b1bd319547f9bc2865
Cherkaoui El Wafi <elwafi.Cherkaoui@gmail.com>
2024-11-27 16:52:19 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // http://api.weatherapi.com/v1/current.json?key=1a0c22a3fdb04c1e8ff214624240310&q=agadir
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
